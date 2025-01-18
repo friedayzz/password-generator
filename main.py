c71dd58e4a482f98ab99906d282b9942c4330045
Daniel Carlgren <hif_daniel@hotmail.com>
2025-01-18 15:21:22 +0100

    Can now generate a random string of 8 characters

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import string
 printables = list(string.printable)
 whitespace = list(string.whitespace)
 
+#Requirements
+min_length = 8
 
 #Take two lists and remove l2 from l1, so in this case we remove whitespace from printables
 def create_list(l1, l2):
@@ -14,16 +16,23 @@ def create_list(l1, l2):
 
     return characters
 
-def generate_password(characters):
+def generate_password(characters, min_len):
+    new_list = []
+    password = ""
     for char in characters:
-        print(char)
-
-
+        rand = random.randrange(0, len(characters))
+        if len(new_list) < min_len:
+            new_list.append(characters[rand])
+    
+    password = "".join(new_list)
+    
+    return password
+    
 
 def main():
     #Create a list with allowed characters to use in the generated password.
     allowed_chars = create_list(printables, whitespace)
 
-    generate_password(allowed_chars)
+    print(generate_password(allowed_chars, min_length))
 
 main()
