4110fdda86ed33cfae38e4c5d0ac25d0120fd9d5
Daniel Carlgren <hif_daniel@hotmail.com>
2025-01-18 19:38:55 +0100

    Changed min_length to just be the actual length of the password

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import random
 import string
 
+def main():
+    printables = list(string.printable)
+    whitespace = list(string.whitespace)
+
+    #Requirements
+    pass_length = 12
+
+    #Create a list with allowed characters to use in the generated password.
+    allowed_chars = create_list(printables, whitespace)
+
+    print(generate_password(allowed_chars, pass_length))
+
 #Take two lists and remove l2 from l1, so in this case we remove whitespace from printables
 def create_list(l1, l2):
     characters = []
@@ -10,13 +22,13 @@ def create_list(l1, l2):
 
     return characters
 
-def generate_password(characters, min_len):
+def generate_password(characters, length):
     new_list = []
     password = ""
     #Loop over the allowed letters, numbers and symbols and pick random characters and join them to a string
     for char in characters:
         rand = random.randrange(0, len(characters))
-        if len(new_list) < min_len:
+        if len(new_list) < length:
             new_list.append(characters[rand])
     
     password = "".join(new_list)
@@ -24,16 +36,5 @@ def generate_password(characters, min_len):
     return password
     
 
-def main():
-    printables = list(string.printable)
-    whitespace = list(string.whitespace)
-
-    #Requirements
-    min_length = 8
-
-    #Create a list with allowed characters to use in the generated password.
-    allowed_chars = create_list(printables, whitespace)
-
-    print(generate_password(allowed_chars, min_length))
 
 main()
