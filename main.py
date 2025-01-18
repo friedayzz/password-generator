bbff4011063f2e837749650cfb1f7b307e3a3cba
Daniel Carlgren <hif_daniel@hotmail.com>
2025-01-18 19:23:35 +0100

    Small updates

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import random
 import string
 
-printables = list(string.printable)
-whitespace = list(string.whitespace)
-
-#Requirements
-min_length = 8
-
 #Take two lists and remove l2 from l1, so in this case we remove whitespace from printables
 def create_list(l1, l2):
     characters = []
@@ -19,6 +13,7 @@ def create_list(l1, l2):
 def generate_password(characters, min_len):
     new_list = []
     password = ""
+    #Loop over the allowed letters, numbers and symbols and pick random characters and join them to a string
     for char in characters:
         rand = random.randrange(0, len(characters))
         if len(new_list) < min_len:
@@ -30,6 +25,12 @@ def generate_password(characters, min_len):
     
 
 def main():
+    printables = list(string.printable)
+    whitespace = list(string.whitespace)
+
+    #Requirements
+    min_length = 8
+
     #Create a list with allowed characters to use in the generated password.
     allowed_chars = create_list(printables, whitespace)
 
